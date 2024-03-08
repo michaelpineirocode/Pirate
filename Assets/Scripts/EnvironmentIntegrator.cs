b9c509b8254398a4c7cb7d7b174b9a8d1fe5b799
Michael Pineiro <michaelpineiro.code@gmail.com>
2024-03-08 14:40:45 -0700

    Working on waves and fixed weird sprite scaling issue when rotating

--- Assets/Scripts/EnvironmentIntegrator.cs
+++ Assets/Scripts/EnvironmentIntegrator.cs
@@ -1,22 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-// This script is in charge of syncing wind direction,
-// wave direction, etc.
 public class EnvironmentIntegrator : MonoBehaviour
 {
     public Vector2 windForceDirection = new Vector2(0, 10);
+    public Transform player;
+    public GameObject waveObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(WaitRandomTimeCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    void instantiateWaves() {
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, windForceDirection);
+        transform.rotation = rotation;
+        Instantiate(waveObject, player.position, rotation);
+    }
+
+    IEnumerator WaitRandomTimeCoroutine()
     {
-        
+        while (true) // Infinite loop to continuously wait and instantiate
+        {
+            // Generate a random time to wait
+            float randomTime = Random.Range(1f, 5f); // Wait between 1 and 5 seconds
+
+            Debug.Log($"Waiting for {randomTime} seconds...");
+            yield return new WaitForSeconds(randomTime); // Wait for the random amount of time
+
+            instantiateWaves(); // Instantiate waves after waiting
+
+            Debug.Log("Wait is over. Executing next actions...");
+        }
     }
 }
